c2720ddb30786329a7b8e2cdaf5c908f65a60bad
hello-world-ttj <tijotjoseph003@gmail.com>
2024-12-21 13:09:18 +0530

    #server

--- src/api/axiosintercepter.js
+++ src/api/axiosintercepter.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3003/api/v1/",
+  baseURL: process.env.VITE_APP_API_URL || "http://localhost:3003/api/v1/",
 });
 axiosInstance.interceptors.request.use(
   (config) => {
