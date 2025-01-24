fba6f373d619a9977526e0959b79e8f5bfa6c9fd
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2025-01-24 16:16:07 +0530

    #posts

--- src/pages/Approvals/MembershipApproval.jsx
+++ src/pages/Approvals/MembershipApproval.jsx
@@ -6,30 +6,33 @@ import RejectEntry from "../../components/Approve/RejectEntry";
 import ApproveApproval from "../../components/Approve/ApproveApproval";
 import { approvalColumns } from "../../assets/json/TableData";
 import { useListStore } from "../../store/listStore";
+import { toast } from "react-toastify";
+import { useProductStore } from "../../store/productStore";
 
 const MembershipApproval = () => {
   const [rejectOpen, setRejectOpen] = useState(false);
   const [approveOpen, setApproveOpen] = useState(false);
   const [isChange, setIsChange] = useState(false);
   const { fetchApproval } = useListStore();
+  const [selectedRows, setSelectedRows] = useState([]);
   const [approvalId, setApprovalId] = useState(null);
   const [search, setSearch] = useState("");
   const [pageNo, setPageNo] = useState(1);
-  const[row, setRow] = useState(10)
-
+  const [row, setRow] = useState(10);
+const {deleteProduct} = useProductStore();
   const handleChange = () => {
     setIsChange((prev) => !prev);
   };
   useEffect(() => {
     let filter = {};
     filter.pageNo = pageNo;
-    filter.limit = row
+    filter.limit = row;
     if (search) {
       filter.search = search;
       setPageNo(1);
     }
     fetchApproval(filter);
-  }, [isChange, pageNo, search,row]);
+  }, [isChange, pageNo, search, row]);
   const handleReject = (id) => {
     setApprovalId(id);
     setRejectOpen(true);
@@ -44,7 +47,21 @@ const MembershipApproval = () => {
   const handleCloseApprove = () => {
     setApproveOpen(false);
   };
-
+  const handleSelectionChange = (newSelectedIds) => {
+    setSelectedRows(newSelectedIds);
+  };
+  const handleDelete = async () => {
+    if (selectedRows.length > 0) {
+      try {
+        await Promise.all(selectedRows?.map((id) => deleteProduct(id)));
+        toast.success("Deleted successfully");
+        setIsChange(!isChange);
+        setSelectedRows([]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <>
       {" "}
@@ -70,11 +87,13 @@ const MembershipApproval = () => {
         <StyledTable
           columns={approvalColumns}
           payment
+          onSelectionChange={handleSelectionChange}
           onModify={handleApprove}
           onAction={handleReject}
           pageNo={pageNo}
           setPageNo={setPageNo}
           rowPerSize={row}
+          onDelete={handleDelete}
           setRowPerSize={setRow}
         />
         <RejectEntry
